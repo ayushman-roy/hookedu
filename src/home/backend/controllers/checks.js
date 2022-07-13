831df10b824f04a3e08cff89b7b6f4ca099fded1
Ayushman Roy <ayushmanroy09@gmail.com>
2022-07-13 23:40:48 +0530

    minor bug fixes

--- src/home/backend/controllers/checks.js
+++ src/home/backend/controllers/checks.js
@@ -2,8 +2,8 @@ export function schoolCheck(value) {
   let campus = ["@ashoka.edu.in", "@jgu.edu.in"];
   let valid = false;
   let email = String(value);
-  for (let school in campus) {
-    valid = email.endsWith(`${school}`);
+  for (let element in campus) {
+    valid = email.endsWith(`${campus[element]}`);
     if (valid) {
       break;
     }
